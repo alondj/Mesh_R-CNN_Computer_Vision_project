174a1f6d8869eec05282b5da09e0a4ac8804509a
alondj <alondej@gmail.com>
2019-08-26 20:37:51 +0300

    utils functions to construct/deconstruct block matrices

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-def conv_output(h, w, kernel=3, padding=0, dilation=1, stride=1):
+from typing import List
+import torch
+from torch import Tensor
+
+
+def conv_output(h: int, w: int, kernel: int = 3, padding: int = 0, dilation: int = 1, stride: int = 1):
     kh, kw = _tuple(kernel)
     ph, pw = _tuple(padding)
     dh, dw = _tuple(dilation)
@@ -6,11 +11,11 @@ def conv_output(h, w, kernel=3, padding=0, dilation=1, stride=1):
     return _dim(h, k=kh, p=ph, s=sh, d=dh), _dim(w, k=kw, p=pw, s=sw, d=dw)
 
 
-def _dim(h, k, p, s, d):
+def _dim(h: int, k: int, p: int, s: int, d: int):
     return int((h+2*p-d*(k-1)-1)/s) + 1
 
 
-def convT_output(h, w, kernel=3, padding=0, dilation=1, stride=1, output_padding=0):
+def convT_output(h: int, w: int, kernel: int = 3, padding: int = 0, dilation: int = 1, stride: int = 1, output_padding: int = 0):
     kh, kw = _tuple(kernel)
     ph, pw = _tuple(padding)
     dh, dw = _tuple(dilation)
@@ -19,7 +24,7 @@ def convT_output(h, w, kernel=3, padding=0, dilation=1, stride=1, output_padding
     return _dimT(h, k=kh, p=ph, s=sh, d=dh, pout=pouth), _dimT(w, k=kw, p=pw, s=sw, d=dw, pout=poutw)
 
 
-def _dimT(h, k, p, s, d, pout):
+def _dimT(h: int, k: int, p: int, s: int, d: int, pout: int):
     return (h-1)*s-2*p+d*(k-1)+pout+1
 
 
@@ -28,3 +33,58 @@ def _tuple(n):
         assert len(n) == 2
         return n
     return n, n
+
+
+def block_matrices(*matrices) -> Tensor:
+    # given multiple matrices of irregular shapes return one squre matrix which contains them all
+    ms = torch.LongTensor([m.shape[0] for m in matrices])
+    ns = torch.LongTensor([m.shape[1] for m in matrices])
+
+    # we do not transpose or reorder in order to have a smaller matrix
+    # maybe later if necessary
+    rows = torch.sum(ms)
+    columns = ns+torch.cumsum(ms, 0)-ms
+
+    N = torch.max(rows, torch.max(columns))
+
+    device = matrices[0].device
+    dtype = matrices[0].dtype
+
+    res = torch.zeros(N, N, device=device, dtype=dtype)
+
+    # TODO vectorize
+    for idx, m in enumerate(matrices):
+        st_r = torch.sum(ms[:idx])
+        c_end = columns[idx]
+
+        res[st_r:st_r+m.shape[0], st_r:c_end] = m
+
+    return res
+
+
+def unblock_matrices(M: Tensor, shapes: list) -> List[Tensor]:
+    sum_rows = 0
+    ms = []
+
+    # TODO vectorize
+    for shape in shapes:
+        m = M[sum_rows:sum_rows+shape[0], sum_rows:sum_rows+shape[1]]
+        ms.append(m)
+        assert m.dtype == M.dtype
+        assert m.device == M.device
+        sum_rows += shape[0]
+
+    return ms
+
+
+if __name__ == "__main__":
+    n_features = 3
+    m1 = torch.arange(10).reshape(2, 5)
+    m2 = torch.arange(9).reshape(3, 3)
+    m3 = torch.arange(6).reshape(2, 3)
+    m4 = torch.arange(8).reshape(2, 4)
+
+    M = block_matrices(m1, m2, m3, m4).to("cuda:0").to(torch.float32)
+    print(M)
+
+    ms = unblock_matrices(M, [(2, 5), (3, 3), (2, 3), (2, 4)])
