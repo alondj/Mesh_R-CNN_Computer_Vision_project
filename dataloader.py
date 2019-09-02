7e3a8a8344651c4f08fc62f0da6d159fdb63d318
benbanuz <benbanuz@users.noreply.github.com>
2019-09-02 18:02:33 +0300

    added pix3d dataset

--- dataloader.py
+++ dataloader.py
@@ -1,27 +1,38 @@
 import json
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from torch.utils.data import Dataset
+import torch
 
 
-# returns 2 lists of imgs and 3d models where the i-th img matches the i-th 3d model
-def get_img_and_model(dataset_path, num_sampels=None):
-    with open(dataset_path) as json_file:
-        dataset = json.load(json_file)
-        models = []
-        imgs = []
+class pix3dDataset(Dataset):
 
-        for i, p in enumerate(dataset):
-            if i == num_sampels and num_sampels is not None:
-                break
-            img_src = f"dataset/pix3d/{p['img']}"
-            img = mpimg.imread(img_src)
-            model3d_src = p["model"]
+    def __init__(self, dataset_path):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+        """
+        with open(dataset_path) as json_file:
+            dataset = json.load(json_file)
+            self.models = []
+            self.imgs = []
 
-            imgs.append(img)
-            models.append(model3d_src)
-        return imgs, models
+            for p in dataset:
+                img_src = f"dataset/pix3d/{p['img']}"
+                img = mpimg.imread(img_src)
+                model3d_src = p["model"]
+
+                self.imgs.append(img)
+                self.models.append(model3d_src)
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return self.imgs[idx], self.models[idx]
 
 
 if __name__ == "__main__":
-    imgs, models = get_img_and_model("dataset/pix3d/pix3d.json", 3)
-    print(models[1])
+    pxd = pix3dDataset("dataset/pix3d/pix3d.json")
