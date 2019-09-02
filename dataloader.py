bc2a8e374750c28a1c7bef10c32dc32349447b0f
benbanuz <benbanuz@users.noreply.github.com>
2019-09-02 18:22:18 +0300

    added pix3d dataset

--- dataloader.py
+++ dataloader.py
@@ -7,31 +7,34 @@ import torch
 class pix3dDataset(Dataset):
 
     def __init__(self, dataset_path):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-        """
         with open(dataset_path) as json_file:
             dataset = json.load(json_file)
-            self.models = []
-            self.imgs = []
+            self.models_src = []
+            self.imgs_src = []
 
             for p in dataset:
                 img_src = f"dataset/pix3d/{p['img']}"
-                img = mpimg.imread(img_src)
-                model3d_src = p["model"]
-
-                self.imgs.append(img)
-                self.models.append(model3d_src)
+                model3d_src = f"dataset/pix3d/{p['model']}"
+                self.imgs_src.append(img_src)
+                self.models_src.append(model3d_src)
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.imgs_src)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        return self.imgs[idx], self.models[idx]
+        img_src = self.imgs_src[idx]
+        model_src = self.models_src[idx]
+
+        imgs = []
+        models = []
+        for img_s, model_s in zip(img_src, model_src):
+            imgs.append(mpimg.imread(img_s))
+            models.append(model_s)
+
+        return imgs, models
 
 
 if __name__ == "__main__":
