be4338087e7213d443c3d0f0027d29abe9747cfc
alondj <alondej@gmail.com>
2019-09-05 13:40:56 +0300

    added modularity to the models

--- models.py
+++ models.py
@@ -1,43 +1,54 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers import VoxelBranch, Cubify, VertixRefinePix3D, VertixRefineShapeNet, ResVertixRefineShapenet
+from layers import VoxelBranch, Cubify, VertixRefinePix3D, VertixRefineShapeNet,\
+    ResVertixRefineShapenet
 from typing import Tuple
 
 
-# TODO add modularity
 class ShapeNetModel(nn.Module):
-    def __init__(self, feature_extractor: nn.Module, residual: bool = True):
+    def __init__(self, feature_extractor: nn.Module, residual: bool = True,
+                 cubify_threshold: float = 0.2, image_shape: Tuple[int, int] = (137, 137),
+                 voxelBranchChannels: Tuple[int, int] = (2048, 48),
+                 alignmenet_channels: int = 3840,
+                 vertex_feature_dim: int = 128,
+                 num_refinement_stages: int = 3):
         super(ShapeNetModel, self).__init__()
         self.feature_extractor = feature_extractor
-        self.voxelBranch = VoxelBranch(2048, 48)
-        self.cubify = Cubify(0.2)
+        self.voxelBranch = VoxelBranch(*voxelBranchChannels)
+        self.cubify = Cubify(cubify_threshold)
 
         refineClass = ResVertixRefineShapenet if residual else VertixRefineShapeNet
 
-        self.refineStage0 = refineClass((137, 137), use_input_features=False)
-        self.refineStage1 = refineClass((137, 137))
-        self.refineStage2 = refineClass((137, 137))
+        self.refineStages = [refineClass(image_shape, alignment_size=alignmenet_channels,
+                                         use_input_features=False,
+                                         num_features=vertex_feature_dim)]
+
+        for _ in range(num_refinement_stages-1):
+            self.refineStages.append(refineClass(image_shape, alignment_size=alignmenet_channels,
+                                                 num_features=vertex_feature_dim,
+                                                 use_input_features=True))
 
     def forward(self, img: torch.Tensor) -> dict:
         img_feature_maps = self.feature_extractor(img)
+
         upscaled = F.interpolate(img_feature_maps[-1], scale_factor=4.8,
                                  mode='bilinear', align_corners=True)
+
         voxelGrid = self.voxelBranch(upscaled)
+
         vertice_index, faces_index, vertex_positions0, edge_index, mesh_faces = self.cubify(
             voxelGrid)
 
-        vertex_features, vertex_positions1 = self.refineStage0(vertice_index, img_feature_maps,
-                                                               edge_index, vertex_positions0)
+        vertex_features, vertex_positions1 = self.refineStages[0](vertice_index, img_feature_maps,
+                                                                  edge_index, vertex_positions0)
 
-        vertex_features, vertex_positions2 = self.refineStage0(vertice_index, img_feature_maps,
-                                                               edge_index, vertex_positions1, vertex_features=vertex_features)
+        vertex_positions = [vertex_positions0, vertex_positions1]
 
-        vertex_features, vertex_positions3 = self.refineStage0(vertice_index, img_feature_maps,
-                                                               edge_index, vertex_positions2, vertex_features=vertex_features)
-
-        vertex_positions = [vertex_positions0, vertex_positions1,
-                            vertex_positions2, vertex_positions3]
+        for stage in self.refineStages[1:]:
+            vertex_features, new_positions = stage(vertice_index, img_feature_maps,
+                                                   edge_index, vertex_positions[-1], vertex_features=vertex_features)
+            vertex_positions.append(new_positions)
 
         output = dict()
         output['vertex_postions'] = vertex_positions
@@ -46,22 +57,32 @@ class ShapeNetModel(nn.Module):
         output['vertice_index'] = vertice_index
         output['faces'] = mesh_faces
         output['voxels'] = voxelGrid
+        output['feature_maps'] = img_feature_maps
 
         return output
 
 
 class Pix3DModel(nn.Module):
-    def __init__(self, feature_extractor: nn.Module, original_image_size: Tuple[int, int]):
+    def __init__(self, image_input_size: Tuple[int, int], feature_extractor: nn.Module,
+                 cubify_threshold: float = 0.2,
+                 voxelBranchChannels: Tuple[int, int] = (256, 24),
+                 alignmenet_channels: int = 256,
+                 vertex_feature_dim: int = 128,
+                 num_refinement_stages: int = 3):
+
         super(Pix3DModel, self).__init__()
         self.feature_extractor = feature_extractor
-        self.voxelBranch = VoxelBranch(256, 24)
-        self.cubify = Cubify(0.2)
-        self.refineStage0 = VertixRefinePix3D(original_image_size,
-                                              use_input_features=False)
-        self.refineStage1 = VertixRefinePix3D(original_image_size,
-                                              use_input_features=True)
-        self.refineStage2 = VertixRefinePix3D(original_image_size,
-                                              use_input_features=True)
+        self.voxelBranch = VoxelBranch(*voxelBranchChannels)
+        self.cubify = Cubify(cubify_threshold)
+
+        self.refineStages = [VertixRefinePix3D(image_input_size, alignment_size=alignmenet_channels,
+                                               use_input_features=False,
+                                               num_features=vertex_feature_dim)]
+
+        for _ in range(num_refinement_stages-1):
+            self.refineStages.append(VertixRefinePix3D(image_input_size, alignment_size=alignmenet_channels,
+                                                       num_features=vertex_feature_dim,
+                                                       use_input_features=True))
 
     def forward(self, image: torch.Tensor) -> dict:
         # TODO for now assume feature extractor handles
@@ -74,17 +95,15 @@ class Pix3DModel(nn.Module):
         vertice_index, faces_index, vertex_positions0, edge_index, mesh_faces = self.cubify(
             voxelGrid)
 
-        vertex_features, vertex_positions1 = self.refineStage0(vertice_index, roiAlign,
-                                                               edge_index, vertex_positions0)
+        vertex_features, vertex_positions1 = self.refineStages[0](vertice_index, roiAlign,
+                                                                  edge_index, vertex_positions0)
 
-        vertex_features, vertex_positions2 = self.refineStage0(vertice_index, roiAlign,
-                                                               edge_index, vertex_positions1, vertex_features=vertex_features)
+        vertex_positions = [vertex_positions0, vertex_positions1]
 
-        vertex_features, vertex_positions3 = self.refineStage0(vertice_index, roiAlign,
-                                                               edge_index, vertex_positions2, vertex_features=vertex_features)
-
-        vertex_positions = [vertex_positions0, vertex_positions1,
-                            vertex_positions2, vertex_positions3]
+        for stage in self.refineStages[1:]:
+            vertex_features, new_positions = stage(vertice_index, roiAlign,
+                                                   edge_index, vertex_positions[-1], vertex_features=vertex_features)
+            vertex_positions.append(new_positions)
 
         output = dict()
         output['vertex_postions'] = vertex_positions
