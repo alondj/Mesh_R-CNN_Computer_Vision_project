883969dd8603f6247d3c6852aa1a174717957c61
benbanuz <benbanuz@users.noreply.github.com>
2019-09-10 00:37:02 +0300

    datasets done

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,9 @@ import matplotlib.image as mpimg
 from torch.utils.data import Dataset
 import scipy.io as sci
 import numpy as np
+from pathlib import Path
+import read_binvox
+
 
 class pix3dDataset(Dataset):
 
@@ -44,14 +47,58 @@ class pix3dDataset(Dataset):
 
             for img_s, model_s, pc_src in zip(img_src, model_src, pointcloud_src):
                 imgs.append(mpimg.imread(img_s))
-                models.append(sci.loadmat(model_s))
+                models.append(sci.loadmat(model_s)['voxel'])
                 clouds.append(np.load(pc_src))
 
             return imgs, models, clouds
 
 
-if __name__ == "__main__":
-    pxd = pix3dDataset("dataset/pix3d/pix3d.json")
-    img, voxs, clouds = pxd[0:3]
-    print(clouds[0])
+class shapeNet_Dataset(Dataset):
 
+    def __init__(self, directory_in_str, num_sampels=None):
+        pathlist = Path(directory_in_str).glob('**/*.binvox')
+        self.imgs_src = []
+        self.models_vox_src = []
+        self.pointcloud = []
+        for i, path in enumerate(pathlist):
+            if num_sampels is not None and i == num_sampels:
+                break
+            voxel_path = str(path)
+            cloud_path = voxel_path.replace("ShapeNetVox32", "ShapeNet_pointclouds")
+            cloud_path = cloud_path.replace("model.binvox", "pointcloud_1024.npy")
+            img_path = voxel_path.replace("ShapeNetVox32", "ShapeNetRendering")
+            img_path = img_path.replace("model.binvox", "rendering/00.png")
+
+            self.models_vox_src.append(voxel_path)
+            self.pointcloud.append(cloud_path)
+            self.imgs_src.append(img_path)
+
+    def __len__(self):
+        return len(self.imgs_src)
+
+    def __getitem__(self, idx):
+        if type(idx) != slice:
+            idx = slice(idx, idx + 1)
+        if type(idx) == slice:
+            img_src = self.imgs_src[idx]
+            model_src = self.models_vox_src[idx]
+            pointcloud_src = self.pointcloud[idx]
+
+            imgs = []
+            models = []
+            clouds = []
+
+            for img_s, model_s, pc_src in zip(img_src, model_src, pointcloud_src):
+                imgs.append(mpimg.imread(img_s))
+                clouds.append(np.load(pc_src))
+                with open(model_s, 'rb') as binvox_file:
+                    models.append(read_binvox.read_as_3d_array(binvox_file))
+
+            return imgs, models, clouds
+
+
+if __name__ == "__main__":
+    # pxd = pix3dDataset("../dataset/pix3d/pix3d.json")
+    sdb = shapeNet_Dataset("../dataset/shapeNet/ShapeNetVox32", 9)
+    imgs, models, clouds = sdb[0:3]
+    print(models[0].shape)
