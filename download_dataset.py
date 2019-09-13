849693846105ab23f8a90215fe93fb1013fda129
benbanuz <benbanuz@users.noreply.github.com>
2019-09-13 14:03:23 +0300

    dataset download instructions

---
+++ download_dataset.py
@@ -0,0 +1,31 @@
+"""
+the dataset should be built like that:
+-dataset
+----shapeNet
+-------ShapeNet_pointclouds
+-------ShapeNetRendering
+-------ShapeNetVox32
+----pix3d
+-------img
+-------mask
+-------model
+-------pointclouds
+-------pix3d.json
+"""
+
+
+def download_pix3d():
+    """
+    pix3d img,voxel,masks http://pix3d.csail.mit.edu/data/pix3d.zip
+    pix3d pointclouds https://drive.google.com/file/d/1RZakyBu9lPbG85SyconBn4sR8r2faInV/view
+    """
+    pass
+
+
+def download_shapenet():
+    """
+    shapeNet imgs http://cvgl.stanford.edu/data2/ShapeNetRendering.tgz
+    shapeNet voxel http://cvgl.stanford.edu/data2/ShapeNetVox32.tgz
+    shapeNet pointclouds https://drive.google.com/open?id=1cfoe521iTgcB_7-g_98GYAqO553W8Y0g
+    """
+    pass
