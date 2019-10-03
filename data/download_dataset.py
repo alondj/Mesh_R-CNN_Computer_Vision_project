9a9f82aa62629cf58607e848d4bdaabd00ce2d67
alondj <alondej@gmail.com>
2019-10-03 17:18:48 +0300

    use a json config for shapenet and dataloading

--- data/download_dataset.py
+++ data/download_dataset.py
@@ -1,15 +1,14 @@
 import urllib.request
 import zipfile
 import os
-import shutil
 import tarfile
 from pathlib import Path
-
+import json
+import argparse
 import torch
 from model.layers import Cubify
 from .read_binvox import read_as_3d_array
 from utils import save_mesh, normalize_mesh
-
 """
 the dataset should be built like that:
 -dataset
@@ -18,6 +17,7 @@ the dataset should be built like that:
 -------ShapeNetRendering
 -------ShapeNetVox32
 -------ShapeNetMeshes
+-------shapenet.json
 ----pix3d
 -------img
 -------mask
@@ -112,6 +112,70 @@ def render_shapenet_meshes(download_path):
                 save_mesh(normalize_mesh(v), f, v_path)
 
 
+def get_shapenet_class_by_name(s: str):
+    if s.find("02691156") != -1:
+        return "airplane"
+    if s.find("02828884") != -1:
+        return "bench"
+    if s.find("02933112") != -1:
+        return "closet"
+    if s.find("02958343") != -1:
+        return "car"
+    if s.find("03001627") != -1:
+        return "chair"
+    if s.find("03211117") != -1:
+        return "tv"
+    if s.find("03636649") != -1:
+        return "lamp"
+    if s.find("03691459") != -1:
+        return "stereo"
+    if s.find("04090263") != -1:
+        return "gun"
+    if s.find("04256520") != -1:
+        return "sofa"
+    if s.find("04379243") != -1:
+        return "table"
+    if s.find("04401088") != -1:
+        return "phone"
+    if s.find("04530566") != -1:
+        return "ship"
+    assert False, "no label found for shapenet should not happen"
+    return -1
+
+
+def create_shapenet_Json(directory_str):
+    json_obj = []
+    pathlist = Path(directory_str).glob('**/*.binvox')
+
+    for path in pathlist:
+        voxel_path = str(path)
+        mesh_src = voxel_path.replace(
+            "ShapeNetVox32", "ShapeNetMeshes")
+        mesh_src.replace(".binvox", ".obj")
+        img_path = voxel_path.replace("ShapeNetVox32", "ShapeNetRendering")
+        img_path = img_path.replace("model.binvox", "rendering/00.png")
+        category = get_shapenet_class_by_name(img_path)
+
+        json_obj.append({"img": img_path, "category": category,
+                         "voxel": voxel_path, "model": mesh_src})
+
+    with open(f"{directory_str}/shapenet.json", "a+") as f:
+        json.dump(json_obj, f)
+
+
+# TODO ben verify this works
+def prepare_shapenet(download_path):
+    download_shapenet(download_path)
+    render_shapenet_meshes(download_path)
+    create_shapenet_Json(download_path)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--download_path', type=str,
+                    help='path to where the datasets will be stored')
+
+
 if __name__ == "__main__":
-    download_pix3d("/home/benbanuz")
-    download_shapenet("/home/benbanuz")
+    download_path = parser.parse_args().download_path
+    download_pix3d(download_path)
+    prepare_shapenet(download_path)
